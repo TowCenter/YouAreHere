1cf5df62b002a1d7fc30320765b01798680badfe
chootka <com@chootka.com>
2016-01-11 03:42:46 -0500

    commented out test fns

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -2,42 +2,51 @@ $( document ).ready( function() {
 
 	console.log("document is ready!");
 
-	Twilio.Device.ready(function (device) {
-              $("#log").text("Ready");
-            });
+	// Twilio.Device.ready(function (device) {
+ //    $("#log").text("Ready");
+ //  });
        
-            Twilio.Device.error(function (error) {
-              $("#log").text("Error: " + error.message);
-            });
+ //  Twilio.Device.error(function (error) {
+ //    $("#log").text("Error: " + error.message);
+ //  });
        
-            Twilio.Device.connect(function (conn) {
-              $("#log").text("Successfully established call");
-            });
+ //  Twilio.Device.connect(function (conn) {
+ //    $("#log").text("Successfully established call");
+ //  });
        
-            Twilio.Device.disconnect(function (conn) {
-              $("#log").text("Call ended");
-            });
+ //  Twilio.Device.disconnect(function (conn) {
+ //    $("#log").text("Call ended");
+ //  });
        
-            Twilio.Device.incoming(function (conn) {
-              $("#log").text("Incoming connection from " + conn.parameters.From);
-              // accept the incoming connection and start two-way audio
-              conn.accept();
-            });
+ //  Twilio.Device.incoming(function (conn) {
+ //    $("#log").text("Incoming connection from " + conn.parameters.From);
+ //    // accept the incoming connection and start two-way audio
+ //    conn.accept();
+ //  });
        
-            function call() {
-              Twilio.Device.connect();
-            }
+ //  function call() {
+ //    Twilio.Device.connect();
+ //  }
        
-            function hangup() {
-              Twilio.Device.disconnectAll();
-            }
+ //  function hangup() {
+ //    Twilio.Device.disconnectAll();
+ //  }
 
-  $(".call").on("click", function() {
-    call();
-  });
+ //  $(".call").on("click", function() {
+ //    call();
+ //  });
 
-  $(".hangup").on("click", function() {
-    hangup();
-  });
+ //  $(".hangup").on("click", function() {
+ //    hangup();
+ //  });
+
+  /* API CALLS >
+
+    https://phiffer.org/youarehere/yah.php?get=stories
+    https://phiffer.org/youarehere/yah.php?get=responses&story=[story ID]
+    https://phiffer.org/youarehere/yah.php?get=mp3s
+    https://phiffer.org/youarehere/yah.php?twilio=1 (Twilio POST endpoint)
+
+  */
 
 } );
